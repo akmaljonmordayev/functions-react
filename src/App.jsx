ded60e2853a12b167efe7e0d6d7d8cb315a08038
akmaljonmordayev <akmaljonmordayev@gmail.com>
2025-03-16 14:50:08 +0500

    functions fully ishladi

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,32 @@ import "./App.css";
 function App() {
   //function declaration
   // function paramets
-  function myFunction(number1, number2, number3) {
-    console.log(number1 + number2 + number3);
-  }
-  myFunction(23, 23, 67);
-  myFunction(23, 45, 67);
-  myFunction(45, 45, 90);
-  myFunction(23, 56, 78);
+  // function myFunction(number1, number2, number3) {
+  //   console.log(number1 + number2 + number3);
+  // }
+  // myFunction(23, 23, 67);
+  // myFunction(23, 45, 67);
+  // myFunction(45, 45, 90);
+  // myFunction(23, 56, 78);
   // funksiya argumants
+
+  function bodyColor(color) {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = color;
+  }
 
   return (
     <>
       <div className="buttons">
-        <button className="button1">Button</button>
-        <button className="button2">Button</button>
-        <button className="button3">Button</button>
+        <button onClick={() => bodyColor("#4ADC2A")} className="button1">
+          Button
+        </button>
+        <button onClick={() => bodyColor("#1B1BE6")} className="button2">
+          Button
+        </button>
+        <button onClick={() => bodyColor("#BA1B1B")} className="button3">
+          Button
+        </button>
       </div>
     </>
   );

27c2593bf1de4739a7c90cea436bc2601ed2abdc
akmaljonmordayev <akmaljonmordayev@gmail.com>
2025-03-16 14:37:52 +0500

    decaration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import "./App.css";
 
 function App() {
+  //function declaration
+  // function paramets
+  function myFunction(number1, number2, number3) {
+    console.log(number1 + number2 + number3);
+  }
+  myFunction(23, 23, 67);
+  myFunction(23, 45, 67);
+  myFunction(45, 45, 90);
+  myFunction(23, 56, 78);
+  // funksiya argumants
+
   return (
     <>
       <div className="buttons">
